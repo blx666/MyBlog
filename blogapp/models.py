11f89964dc6b870d4498acc3bb7d2773dfc51829
= <=>
2017-02-28 18:19:57 +0800

    my blog3

--- blogapp/models.py
+++ blogapp/models.py
@@ -34,9 +34,9 @@ class Comment(models.Model):
         verbose_name_plural = 'Comments'
 
 
-
 from pygments.lexers import get_all_lexers
 from pygments.styles import get_all_styles
+
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
